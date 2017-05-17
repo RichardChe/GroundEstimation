8600c58ae692fbf98df764b24b2e4c32467c1ce4
Yuechao Xie <576565773@qq.com>
2017-05-17 15:46:17 +0800

    fix size problem

--- src/Visualizer.cpp
+++ src/Visualizer.cpp
@@ -9,15 +9,17 @@ Visualizer::~Visualizer(){}
 
 Mat Visualizer::showGroundWithImage(Scalar color)
 {
-	int width =  mGroundEstimator->mLeftImg->cols;
-	int height = mGroundEstimator->mRightImg->rows;
+	int width  = mGroundEstimator->getGroundMap().cols;
+	int height = mGroundEstimator->getGroundMap().rows;
 	Mat result(height, width, CV_8UC3);
 
 	Mat mask(height,width,CV_8UC3);
 	Mat LeftImgBGR;
 	cvtColor(*mGroundEstimator->mLeftImg, LeftImgBGR, CV_GRAY2BGR);
+	resize(LeftImgBGR, LeftImgBGR, Size(width, height));
+
 	mask.setTo(color, mGroundEstimator->getGroundMap() == 0);
-	addWeighted(mask, 0.5, LeftImgBGR, 0.5,0,result);
+	addWeighted(mask, 0.3, LeftImgBGR, 0.7,0,result);
 
 	return result;
 }
